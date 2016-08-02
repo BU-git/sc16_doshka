70a125c815313351bd75b240fd2232a6a2a8c39e
Юлія Гузенкова < >
2016-08-03 02:05:31 +0300

    audioplayer

---
+++ src/js/audio.js
@@ -0,0 +1,20 @@
+var music = document.getElementById('audio_player');
+  
+function playAudio() {
+  if (music.paused) {
+    music.play();
+    pButton.className = "";
+    pButton.className = "pause";
+  } else {
+    music.pause();
+    pButton.className = "";
+    pButton.className = "play";
+  }
+  {
+ 		console.log (playAudio);
+	}
+}
+
+function setVolume(volume) {
+   music.volume = volume;
+}
