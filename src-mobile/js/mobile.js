4526d695159f1aed1d4b864ffaa389c0f0184ad0
Bogdan Leonchyk <b.leonchyk@gmail.com>
2016-08-10 12:56:25 +0300

    orientation fixed

--- src-mobile/js/mobile.js
+++ src-mobile/js/mobile.js
@@ -1,11 +1,14 @@
-$(document).ready(function () {
-	$('body').append('<h1>Переверните пожалуйста обратно.</h1>');
-	$('h1').css({
-		'text-align' : 'center',
-		'color' : 'white',
-		'margin-top' : '-20%',
-	})
-});
+// $(window).on("orientationchange",function(){
+// 	if(window.orientation == 0)
+// 	{
+// 	  // $('body').text("The orientation has changed to portrait!").css({"background-color":"yellow","font-size":"300%"});
+// 	}
+// 	else
+// 	{
+// 	  // $('body').text("The orientation has changed to landscape!").css({"background-color":"pink","font-size":"200%"});
+// 	}
+// });
+
 $(function () {
 	var btn = $('.nav_btn, .search_btn');
 	btn.click(function(){
